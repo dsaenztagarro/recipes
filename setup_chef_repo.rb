57692901fd25d4bacaa9d4aa21ba9a4e97ba932d
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-10-05 02:48:48 +0200

    Initial commit

--- setup_chef_repo.rb
+++ setup_chef_repo.rb
@@ -1,17 +1,14 @@
 # workstation
-local_filepath = node['chef-repo']['filepath']
+repo_filepath = node['chef-repo']['filepath']
 
 # remote server
-hostname = node['chef-server']['hostname']
-username = node['chef-server']['username']
-permissions_filepath = node['chef-server']['permissions_filepath']
-remote_filepath = "#{username}@#{hostname}:/#{permissions_filepath}"
+cookbook_filepath = node['chef-server-cookbook']['permissions_filepath']
 
 bash 'get_config_files' do
-  cwd ::File.dirname(local_filepath)
+  cwd ::File.dirname(repo_filepath)
   code <<-EOH
     mkdir -p .chef
-    scp -p vagrant #{remote_filepath}/userlocal.pem #{local_filepath}/.chef
-    scp -p vagrant #{remote_filepath}/mychef.pem #{local_filepath}/.chef
+    cp -p vagrant #{cookbook_filepath}/userlocal.pem .chef
+    cp -p vagrant #{cookbook_filepath}/mychef.pem .chef
   EOH
 end
