8971c59c13a022d9d0c6edea8e2358d402070dc9
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-10-05 03:10:16 +0200

    Fixed errors

--- setup_chef_repo_local.rb
+++ setup_chef_repo_local.rb
@@ -1,14 +1,16 @@
 # workstation
 repo_filepath = node['chef-repo']['filepath']
 
+print repo_filepath
+
 # remote server
-cookbook_filepath = node['chef-server-cookbook']['permissions_filepath']
+cookbook_filepath = node['chef-server-cookbook']['filepath']
 
 bash 'get_config_files' do
-  cwd ::File.dirname(repo_filepath)
+  cwd repo_filepath
   code <<-EOH
     mkdir -p .chef
-    cp -p vagrant #{cookbook_filepath}/userlocal.pem .chef
-    cp -p vagrant #{cookbook_filepath}/mychef.pem .chef
+    cp -f #{cookbook_filepath}/permissions/userlocal.pem .chef/
+    cp -f #{cookbook_filepath}/permissions/orglocal-validator.pem .chef/
   EOH
 end
